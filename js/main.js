abd879ce552cfe9e0a935bedfeb209019ec801e2
Surbhi <ssharma281@usf.edu>
2022-03-10 22:24:15 -0500

    Intial Web-Look-Feel

--- js/main.js
+++ js/main.js
@@ -5,10 +5,16 @@
 /**
  * Sample code
  */
-document.addEventListener('DOMContentLoaded', function() {
-  var ulElement = document.getElementById('links');
-  var liElement = document.createElement('li');
-  liElement.appendChild(document.createTextNode('created by main.js'));
+//document.addEventListener('DOMContentLoaded', function() {
+//  var ulElement = document.getElementById('links');
+//  var liElement = document.createElement('li');
+//  liElement.appendChild(document.createTextNode('created by main.js'));
 
-  ulElement.appendChild(liElement);
-});
+//  ulElement.appendChild(liElement);
+//});
+const menu = document.querySelector('#mobile_menu');
+const menuLinks = document.querySelector('.navbar_menu');
+menu.addEventListener('click', function () {
+    menu.classList.toggle('is_active');
+    menuLinks.classList.toggle('active');
+})
